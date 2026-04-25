836be8e308b24a8f965583490e485d2bb425d92e
root <root@srv1603996.hstgr.cloud>
2026-04-25 19:28:19 +0700

    add: PWA + fix nginx css + remove mobile chat duplicate

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'ddsh-v1';
+const STATIC_CACHE = [
+  '/home',
+  '/public/css/global.css',
+  '/public/js/global.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
